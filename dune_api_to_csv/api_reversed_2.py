d64d8573a938d6d92fbe78bade25ea2f4f740fda
Marco Germanier <marco@mrgt.me>
2022-09-22 22:17:43 +0100

    added working api v2

--- dune_api_to_csv/api_reversed_2.py
+++ dune_api_to_csv/api_reversed_2.py
@@ -35,12 +35,12 @@ result_id = json.loads(response.text)['data']['get_result_v2']['result_id']
 
 # second query
 
-payload = "{\"operationName\":\"FindResultDataByResult\",\"variables\":{\"result_id\":\"43344030-f2dc-452f-b833-6799fc2a27ce\",\"error_id\":\"00000000-0000-0000-0000-000000000000\"},\"query\":\"query FindResultDataByResult($result_id: uuid!, $error_id: uuid!) {\\n  query_results(where: {id: {_eq: $result_id}}) {\\n    id\\n    job_id\\n    runtime\\n    generated_at\\n    columns\\n    __typename\\n  }\\n  query_errors(where: {id: {_eq: $error_id}}) {\\n    id\\n    job_id\\n    runtime\\n    message\\n    metadata\\n    type\\n    generated_at\\n    __typename\\n  }\\n  get_result_by_result_id(args: {want_result_id: $result_id}) {\\n    data\\n    __typename\\n  }\\n}\\n\"}"
+payload = "{\"operationName\":\"FindResultDataByResult\",\"variables\":{\"result_id\":\""+result_id+"\",\"error_id\":\"00000000-0000-0000-0000-000000000000\"},\"query\":\"query FindResultDataByResult($result_id: uuid!, $error_id: uuid!) {\\n  query_results(where: {id: {_eq: $result_id}}) {\\n    id\\n    job_id\\n    runtime\\n    generated_at\\n    columns\\n    __typename\\n  }\\n  query_errors(where: {id: {_eq: $error_id}}) {\\n    id\\n    job_id\\n    runtime\\n    message\\n    metadata\\n    type\\n    generated_at\\n    __typename\\n  }\\n  get_result_by_result_id(args: {want_result_id: $result_id}) {\\n    data\\n    __typename\\n  }\\n}\\n\"}"
 headers = {
   'x-hasura-api-key': '',
   'Content-Type': 'text/plain'
 }
 
-response = requests.request("POST", url, headers=headers, data=payload)
+response_2 = requests.request("POST", url, headers=headers, data=payload)
 
-print(response.text)
+print(response_2.text)
